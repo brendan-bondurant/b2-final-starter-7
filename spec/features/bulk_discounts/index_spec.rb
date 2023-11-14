844f04de7c551ec5301d2f9c13aeb96bae2ef561
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-13 20:30:59 -0700

    one more edge case

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -89,6 +89,19 @@ RSpec.describe "bulk discount index" do
       expect(page).to have_content("50%")
       expect(page).to have_content(80)
     end
+    it 'lets you create a new discount with a string' do
+      expect(page).to have_link("New Discount")
+      click_link("New Discount")
+      expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant1.id))
+
+      fill_in "bulk_discount[percentage]", with: "50%"
+      fill_in "bulk_discount[quantity]", with: 80
+      click_button "Submit"
+
+      expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1.id))
+      expect(page).to have_content("50%")
+      expect(page).to have_content(80)
+    end
   end
 
   describe 'destroy#discount' do
